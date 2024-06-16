96dac74a4d2c18c0a78c08fab9133217330becaf
Shlok <shlokm25@gmail.com>
2024-06-17 00:06:56 +0530

    Using POST & PUT Mapping in REST API

--- springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -1,10 +1,8 @@
 package net.javaguides.springboot.controller;
 
 import net.javaguides.springboot.bean.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,5 +35,25 @@ public class StudentController {
     @GetMapping("students/query")
     public Student studentRequestVariable(@RequestParam int id, @RequestParam String firstName, @RequestParam String lastName) {
         return new Student(id, firstName, lastName);
+    }
+
+    //Spring Boot REST API to handle HTTP POST Request
+    @PostMapping("students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student) {
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
+    }
+
+    //Spring Boot API to handle HTTP PUT Request - update the existing resource
+
+    @PutMapping("student/{id}/update")
+    public Student updateStudent(@RequestBody Student student, @PathVariable int id) {
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
     }
 }
