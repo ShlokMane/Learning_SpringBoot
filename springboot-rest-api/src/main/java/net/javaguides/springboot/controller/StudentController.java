b5c11837f980c7df6e9262ffd2a73098b9e72f05
Shlok <shlokm25@gmail.com>
2024-06-16 19:35:46 +0530

    Return List in JSON Format in Get API

--- springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -4,6 +4,9 @@ import net.javaguides.springboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -11,5 +14,13 @@ public class StudentController {
     public Student student() {
         Student student = new Student(7, "Shlok", "Mane");
         return student;
+    }
+
+    @GetMapping("all-students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add( new Student(7, "Shlok", "Mane"));
+        students.add( new Student(9, "John ", "Doe"));
+        return students;
     }
 }
