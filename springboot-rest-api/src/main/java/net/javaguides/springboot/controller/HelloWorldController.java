bc28e124f5a166da5a2742e1a249c15fdd08e24c
Shlok <shlokm25@gmail.com>
2024-06-16 17:02:50 +0530

    Creating First GET API

---
+++ springboot-rest-api/src/main/java/net/javaguides/springboot/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package net.javaguides.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
