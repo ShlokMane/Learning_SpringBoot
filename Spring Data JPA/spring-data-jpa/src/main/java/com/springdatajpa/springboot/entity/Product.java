11bf5a59ce052c0b522e89bba6abe8f410845d5d
Shlok <shlokm25@gmail.com>
2024-06-19 19:55:13 +0530

    Learnt About @GeneratedValue, @SequenceGenerator, @CreationTimestamp & @UpdateTimestamp

--- Spring Data JPA/spring-data-jpa/src/main/java/com/springdatajpa/springboot/entity/Product.java
+++ Spring Data JPA/spring-data-jpa/src/main/java/com/springdatajpa/springboot/entity/Product.java
@@ -1,18 +1,40 @@
 package com.springdatajpa.springboot.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "products", schema = "ecommerce", uniqueConstraints = {
+        @UniqueConstraint(
+                name = "sku_unique",
+                columnNames = "stock_keeping_unit"
+        )
+})
 public class Product {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_sequence")
+    @SequenceGenerator(name = "product_generator", sequenceName = "product_sequence_name", allocationSize = 2)
     private Long id;
+
+    @Column(name = "stock_keeping_unit", nullable = false)
     private String sku;
+
+    @Column(nullable = false)
     private String name;
+
+    private String description;
+    private BigDecimal price;
+    private boolean active;
+    private String imageUrl;
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+    @UpdateTimestamp
+    private LocalDateTime lastUpdated;
 
     public Long getId() {
         return id;
@@ -85,11 +107,4 @@ public class Product {
     public void setLastUpdated(LocalDateTime lastUpdated) {
         this.lastUpdated = lastUpdated;
     }
-
-    private String description;
-    private BigDecimal price;
-    private boolean active;
-    private String imageUrl;
-    private LocalDateTime dateCreated;
-    private LocalDateTime lastUpdated;
 }
