4ab3e91046332ee46e30adb8cebd9e3773624e1b
Shlok <shlokm25@gmail.com>
2024-06-20 01:10:53 +0530

    Created a Product Repository Interface

---
+++ Spring Data JPA/spring-data-jpa/src/main/java/com/springdatajpa/springboot/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.springdatajpa.springboot.repository;
+
+import com.springdatajpa.springboot.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
