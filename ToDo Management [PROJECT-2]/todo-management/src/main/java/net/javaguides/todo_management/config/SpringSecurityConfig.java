cfebcf47a3ada647600a3a46b01bb6f1360f609f
Shlok <shlokm25@gmail.com>
2024-07-18 07:19:49 +0530

    Add Spring Security and Set Custom Password

---
+++ ToDo Management [PROJECT-2]/todo-management/src/main/java/net/javaguides/todo_management/config/SpringSecurityConfig.java
@@ -0,0 +1,41 @@
+package net.javaguides.todo_management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf((csrf) -> csrf.disable()).authorizeHttpRequests((authorize) -> {
+            authorize.anyRequest().authenticated();
+        }).httpBasic(Customizer.withDefaults());
+
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails shlok = User.builder().username("shlok").password(passwordEncoder().encode("password")).roles("USER").build();
+        UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode("admin")).roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(shlok, admin);
+    }
+}
