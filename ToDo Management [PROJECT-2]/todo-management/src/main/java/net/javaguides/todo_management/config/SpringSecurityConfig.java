0f1a89f8273b9a2f3cc5d1fc75bf52eee1c00145
Shlok <shlokm25@gmail.com>
2024-07-18 07:46:06 +0530

    Add Role Based Authorization

--- ToDo Management [PROJECT-2]/todo-management/src/main/java/net/javaguides/todo_management/config/SpringSecurityConfig.java
+++ ToDo Management [PROJECT-2]/todo-management/src/main/java/net/javaguides/todo_management/config/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package net.javaguides.todo_management.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -24,6 +25,11 @@ public class SpringSecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity.csrf((csrf) -> csrf.disable()).authorizeHttpRequests((authorize) -> {
+            authorize.requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN");
+            authorize.requestMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN");
+            authorize.requestMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN");
+            authorize.requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN", "USER");
+            authorize.requestMatchers(HttpMethod.PATCH, "/api/**").hasAnyRole("ADMIN", "USER");
             authorize.anyRequest().authenticated();
         }).httpBasic(Customizer.withDefaults());
 
