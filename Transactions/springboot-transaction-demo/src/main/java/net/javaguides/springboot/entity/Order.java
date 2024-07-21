edecbed66106eca2ee411cc2842dded667eeb96a
Shlok <shlokm25@gmail.com>
2024-07-21 19:54:23 +0530

    Create Order And Payment Entity

---
+++ Transactions/springboot-transaction-demo/src/main/java/net/javaguides/springboot/entity/Order.java
@@ -0,0 +1,32 @@
+package net.javaguides.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderTrackingNumber;
+    private int totalQuantity;
+    private BigDecimal totalPrice;
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+    @UpdateTimestamp
+    private LocalDateTime lastUpdateTime;
+    private Long shoppingCartId;
+}
