178dcfc9f0b9f6e9ce71db9704634911896d4779
Shlok <shlokm25@gmail.com>
2024-07-23 07:13:20 +0530

    Initial Commit for Search REST API

---
+++ Search REST Api/spring-boot-search-rest-api/src/main/java/net/javaguides/springboot/SpringBootSearchRestApiApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSearchRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSearchRestApiApplication.class, args);
+	}
+
+}
